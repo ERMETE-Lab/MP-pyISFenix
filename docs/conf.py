7b9da8a2aab6252d6719b3d85a3faa0d49556d41
Steriva <stefano18.riva@mail.polimi.it>
2024-06-26 11:01:40 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -76,3 +76,4 @@ autodoc_mock_imports = ["dolfinx", "ufl", "petsc4py", "pyvista", "h5py"]
 # html_theme = 'sphinx_rtd_theme'
 html_theme = 'pydata_sphinx_theme'
 # html_theme = 'sphinx_book_theme'
+# html_theme = 'sphinx_pdj_theme'
